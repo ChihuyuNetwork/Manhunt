bc168afe245b53b554c28f965c330a2a6df8b17e
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-12-30 17:20:02 +0900

    ミッション完了を指定する引数を追加

--- src/main/java/love/chihuyu/commands/ManhuntEnd.kt
+++ src/main/java/love/chihuyu/commands/ManhuntEnd.kt
@@ -1,6 +1,7 @@
 package love.chihuyu.commands
 
 import dev.jorel.commandapi.CommandAPICommand
+import dev.jorel.commandapi.arguments.BooleanArgument
 import dev.jorel.commandapi.executors.PlayerCommandExecutor
 import love.chihuyu.Plugin
 import love.chihuyu.game.GameManager
@@ -9,9 +10,10 @@ import net.kyori.adventure.text.Component
 object ManhuntEnd {
 
     val main: CommandAPICommand = CommandAPICommand("end")
+        .withArguments(BooleanArgument("missioned"))
         .executesPlayer(
-            PlayerCommandExecutor { _, _ ->
-                GameManager.end(false)
+            PlayerCommandExecutor { _, args ->
+                GameManager.end(args[0] as Boolean)
                 Plugin.plugin.server.broadcast(Component.text("${Plugin.prefix} ゲームが終了されました"))
             }
         )
