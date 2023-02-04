f0964e656ac5c035b26035e5ab9027f21825f6a5
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2023-02-05 02:11:30 +0900

    ボスバーの軽量化

--- src/main/java/love/chihuyu/utils/BossbarUtil.kt
+++ src/main/java/love/chihuyu/utils/BossbarUtil.kt
@@ -3,6 +3,8 @@ package love.chihuyu.utils
 import love.chihuyu.Plugin.Companion.plugin
 import love.chihuyu.game.GameManager
 import love.chihuyu.game.ManhuntMission
+import love.chihuyu.utils.TeamUtil.isHunter
+import love.chihuyu.utils.TeamUtil.isRunner
 import org.bukkit.Bukkit
 import org.bukkit.NamespacedKey
 import org.bukkit.boss.BarColor
@@ -12,15 +14,16 @@ object BossbarUtil {
 
     fun updateBossbar(mission: ManhuntMission) {
         plugin.server.onlinePlayers.forEach {
-            val key = NamespacedKey(plugin, "manhunt-${it.uniqueId}")
-            val bossbar = Bukkit.getBossBar(key) ?: Bukkit.createBossBar(key, "残り時間", BarColor.RED, BarStyle.SOLID)
+            val key = NamespacedKey(plugin, if (it.isRunner()) "manhunt-runner" else "manhunt-hunter")
+            val bossbar = plugin.server.getBossBar(key) ?: Bukkit.createBossBar(key, "残り時間", BarColor.RED, BarStyle.SOLID)
             val remains = GameManager.endEpoch - EpochUtil.nowEpoch()
-            val msg = if (it in GameManager.hunters()) "ランナーを全員殺せ" else mission.msg
+            val msg = if (it.isHunter()) "ランナーを全員殺せ" else mission.msg
 
             bossbar.setTitle("$msg ≫ ${EpochUtil.formatTime(remains)}")
             bossbar.isVisible = true
+            bossbar.progress = (remains * (1.0 / (GameManager.endEpoch - GameManager.startEpoch))).coerceIn(.0..1.0)
+
             bossbar.removeAll()
-            bossbar.progress = remains * +(1.0 / (GameManager.endEpoch - GameManager.startEpoch))
             bossbar.addPlayer(it)
         }
     }
