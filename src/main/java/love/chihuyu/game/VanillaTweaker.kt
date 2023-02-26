dc5cb828a347290717331106b572b5a3b2620822
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2023-02-26 15:56:38 +0900

    ピグリン交易の確率を独自実装にした

--- src/main/java/love/chihuyu/game/VanillaTweaker.kt
+++ src/main/java/love/chihuyu/game/VanillaTweaker.kt
@@ -1,15 +1,62 @@
 package love.chihuyu.game
 
 import com.destroystokyo.paper.event.player.PlayerPostRespawnEvent
+import love.chihuyu.utils.ItemUtil
+import org.bukkit.ChatColor
+import org.bukkit.Color
+import org.bukkit.Material
+import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.EnderSignal
+import org.bukkit.entity.Piglin
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityDropItemEvent
 import org.bukkit.event.entity.EntitySpawnEvent
 import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.inventory.ItemStack
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
+import kotlin.random.Random
 
 object VanillaTweaker: Listener {
+
+    class CustomBartarItem(val weight: Int, val items: List<() -> ItemStack>)
+
+    private val bartarList = listOf(
+        CustomBartarItem(5, listOf
+            { ItemUtil.createEnchantBook(storedEnchants = mapOf(Pair(Enchantment.SOUL_SPEED, Random.nextInt(1, 4))), amount = 1) }
+        ),
+        CustomBartarItem(8, listOf
+            { ItemUtil.create(Material.IRON_BOOTS, enchantments = mapOf(Pair(Enchantment.SOUL_SPEED, Random.nextInt(1, 4))), amount = 1) }
+        ),
+        CustomBartarItem(10, listOf(
+            { ItemUtil.create(Material.IRON_NUGGET, amount = Random.nextInt(9, 37)) },
+            { ItemUtil.createPotion("${ChatColor.RESET}耐火のポーション", customEffects = listOf(PotionEffect(PotionEffectType.FIRE_RESISTANCE, 180 * 20, 0)), amount = 1, color = Color.ORANGE) },
+            { ItemUtil.createSplashPotion("${ChatColor.RESET}耐火のスプラッシュポーション", customEffects = listOf(PotionEffect(PotionEffectType.FIRE_RESISTANCE, 180 * 20, 0)), amount = 1, color = Color.ORANGE) })
+        ),
+        CustomBartarItem(20, listOf(
+            { ItemUtil.create(Material.QUARTZ, amount = Random.nextInt(8, 17)) },
+            { ItemUtil.create(Material.GLOWSTONE_DUST, amount = Random.nextInt(5, 13)) },
+            { ItemUtil.create(Material.MAGMA_CREAM, amount = Random.nextInt(2, 7)) },
+            { ItemUtil.create(Material.ENDER_PEARL, amount = Random.nextInt(4, 9)) },
+            { ItemUtil.create(Material.STRING, amount = Random.nextInt(8, 25)) })
+        ),
+        CustomBartarItem(40, listOf(
+            { ItemUtil.create(Material.FIRE_CHARGE, amount = Random.nextInt(1, 6)) },
+            { ItemUtil.create(Material.GRAVEL, amount = Random.nextInt(8, 17)) },
+            { ItemUtil.create(Material.LEATHER, amount = Random.nextInt(4, 11)) },
+            { ItemUtil.create(Material.NETHER_BRICK, amount = Random.nextInt(4, 17)) },
+            { ItemUtil.create(Material.OBSIDIAN, amount = Random.nextInt(1, 3)) },
+            { ItemUtil.create(Material.CRYING_OBSIDIAN, amount = Random.nextInt(1, 4)) },
+            { ItemUtil.create(Material.SOUL_SAND, amount = Random.nextInt(4, 17)) })
+        )
+    ).flatMap { customBartarItem ->
+        val list = mutableListOf<() -> ItemStack>()
+        repeat(customBartarItem.weight) {
+            customBartarItem.items.forEach { list.add(it) }
+        }
+        return@flatMap list
+    }
 
     @EventHandler
     fun onRespawn(e: PlayerPostRespawnEvent) {
@@ -28,5 +75,13 @@ object VanillaTweaker: Listener {
         val entity = e.entity
         if (entity !is EnderSignal) return
         entity.dropItem = true
+    }
+
+    @EventHandler
+    fun onDrop(e: EntityDropItemEvent) {
+        val entity = e.entity
+        if (entity !is Piglin) return
+
+        e.itemDrop.itemStack = bartarList[Random.nextInt(1, 423)].invoke()
     }
 }
--- src/main/java/love/chihuyu/utils/ItemUtil.kt
+++ src/main/java/love/chihuyu/utils/ItemUtil.kt
@@ -1,5 +1,6 @@
 package love.chihuyu.utils
 
+import org.bukkit.Color
 import org.bukkit.Material
 import org.bukkit.attribute.Attribute
 import org.bukkit.attribute.AttributeModifier
@@ -7,8 +8,83 @@ import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemFlag
 import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.EnchantmentStorageMeta
+import org.bukkit.inventory.meta.PotionMeta
+import org.bukkit.potion.PotionData
+import org.bukkit.potion.PotionEffect
 
 object ItemUtil {
+
+    fun createSplashPotion(
+        name: String? = null,
+        localizedName: String? = null,
+        amount: Int? = null,
+        customModelData: Int? = null,
+        lore: List<String>? = null,
+        attributeModifier: Map<Attribute, AttributeModifier>? = null,
+        enchantments: Map<Enchantment, Int>? = null,
+        flags: List<ItemFlag>? = null,
+        unbreakable: Boolean? = null,
+        customEffects: List<PotionEffect>? = null,
+        color: Color? = null,
+        basePotionData: PotionData? = null,
+    ): ItemStack {
+        val item = create(
+            Material.SPLASH_POTION, name, localizedName, amount, customModelData, lore, attributeModifier, enchantments, flags, unbreakable
+        )
+        val meta = item.itemMeta as PotionMeta
+        customEffects?.forEach { meta.addCustomEffect(it, true) }
+        meta.color = color
+        if (basePotionData != null) meta.basePotionData = basePotionData
+        item.itemMeta = meta
+        return item
+    }
+
+    fun createPotion(
+        name: String? = null,
+        localizedName: String? = null,
+        amount: Int? = null,
+        customModelData: Int? = null,
+        lore: List<String>? = null,
+        attributeModifier: Map<Attribute, AttributeModifier>? = null,
+        enchantments: Map<Enchantment, Int>? = null,
+        flags: List<ItemFlag>? = null,
+        unbreakable: Boolean? = null,
+        customEffects: List<PotionEffect>? = null,
+        color: Color? = null,
+        basePotionData: PotionData? = null,
+    ): ItemStack {
+        val item = create(
+            Material.POTION, name, localizedName, amount, customModelData, lore, attributeModifier, enchantments, flags, unbreakable
+        )
+        val meta = item.itemMeta as PotionMeta
+        customEffects?.forEach { meta.addCustomEffect(it, true) }
+        meta.color = color
+        if (basePotionData != null) meta.basePotionData = basePotionData
+        item.itemMeta = meta
+        return item
+    }
+
+    fun createEnchantBook(
+        name: String? = null,
+        localizedName: String? = null,
+        amount: Int? = null,
+        customModelData: Int? = null,
+        lore: List<String>? = null,
+        attributeModifier: Map<Attribute, AttributeModifier>? = null,
+        enchantments: Map<Enchantment, Int>? = null,
+        flags: List<ItemFlag>? = null,
+        unbreakable: Boolean? = null,
+        storedEnchants: Map<Enchantment, Int>? = null,
+    ): ItemStack {
+        val item = create(
+            Material.ENCHANTED_BOOK, name, localizedName, amount, customModelData, lore, attributeModifier, enchantments, flags, unbreakable
+        )
+        val meta = item.itemMeta as EnchantmentStorageMeta
+        storedEnchants?.forEach { meta.addStoredEnchant(it.key, it.value, true) }
+        item.itemMeta = meta
+        return item
+    }
 
     fun create(
         material: Material,
@@ -33,10 +109,10 @@ object ItemUtil {
         if (customModelData != null) meta.setCustomModelData(customModelData)
 
         attributeModifier?.forEach { meta.addAttributeModifier(it.key, it.value) }
-        enchantments?.forEach { item.addUnsafeEnchantment(it.key, it.value) }
         flags?.forEach { meta.addItemFlags(it) }
 
         item.itemMeta = meta
+        enchantments?.forEach { item.addUnsafeEnchantment(it.key, it.value) }
         return item
     }
 
