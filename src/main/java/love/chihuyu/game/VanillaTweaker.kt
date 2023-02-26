d143503240a0ae25a02afcbd63304d37b40f94b7
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2023-02-26 14:08:59 +0900

    エンダーアイが壊れないようにした

--- src/main/java/love/chihuyu/game/VanillaTweaker.kt
+++ src/main/java/love/chihuyu/game/VanillaTweaker.kt
@@ -1,8 +1,10 @@
 package love.chihuyu.game
 
 import com.destroystokyo.paper.event.player.PlayerPostRespawnEvent
+import org.bukkit.entity.EnderSignal
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntitySpawnEvent
 import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
@@ -18,7 +20,13 @@ object VanillaTweaker: Listener {
     @EventHandler
     fun onDeath(e: PlayerDeathEvent) {
         val player = e.entity
-
         player.addPotionEffect(PotionEffect(PotionEffectType.NIGHT_VISION, Int.MAX_VALUE, 0, false, false, false))
+    }
+
+    @EventHandler
+    fun onThrow(e: EntitySpawnEvent) {
+        val entity = e.entity
+        if (entity !is EnderSignal) return
+        entity.dropItem = true
     }
 }
